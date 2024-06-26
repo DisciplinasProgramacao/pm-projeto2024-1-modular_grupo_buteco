63a8997ceafd14f72c2e75e8de29f8180d7cfac5
erica-as <erica.asnt@hotmail.com>
2024-06-25 23:09:47 -0300

    create classe pedido #7

---
+++ src/Buteco/src/main/java/org/example/Pedido.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public abstract class Pedido {
+    protected List<Item> itens;
+
+    public Pedido() {
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(Item item) {
+        itens.add(item);
+    }
+
+    public abstract double calcularPreco();
+
+    public List<Item> getItens() {
+        return itens;
+    }
+}
